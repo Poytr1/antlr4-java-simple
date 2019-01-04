9fece79d2685953ca2456a0abf3e80df587cc9b5
Poytr1 <pengcheng.xu@elastic.co>
2019-01-04 23:18:58 +0800

    use json object

--- src/main/java/com/poytr1/antlr4java/Main.java
+++ src/main/java/com/poytr1/antlr4java/Main.java
@@ -13,6 +13,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
 
+import org.json.JSONObject;
 /**
  * @author Poytr1
  */
@@ -22,32 +23,45 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Antlr4 Java Simple");
 
-        map.put("classes", new ArrayList<>());
-        map.put("methods", new ArrayList<>());
-        map.put("fields", new ArrayList<>());
-        map.put("constructors", new ArrayList<>());
-
+        ArrayList<Map<String, Object>> elements = new ArrayList<>();
         class ExtendedJava8Visitor extends Java8BaseVisitor<Void> {
 
             @Override
             public Void visitClassDeclaration(Java8Parser.ClassDeclarationContext ctx) { 
-                map.get("classes").add(ctx.normalClassDeclaration().Identifier().getText());
+                String name = ctx.normalClassDeclaration().Identifier().getText();
+                int line = ctx.normalClassDeclaration().Identifier().getSymbol().getLine();
+                int start = ctx.normalClassDeclaration().Identifier().getSymbol().getCharPositionInLine();  
+                int end = ctx.normalClassDeclaration().Identifier().getSymbol().getCharPositionInLine() + 
+                    ctx.normalClassDeclaration().Identifier().getSymbol().getStopIndex() - ctx.normalClassDeclaration().Identifier().getSymbol().getStartIndex();  
+                addElement(name, line, start, end, "class", elements);
                 return visitChildren(ctx); 
             }
         
             @Override public Void visitVariableDeclarator(Java8Parser.VariableDeclaratorContext ctx) { 
-                map.get("fields").add(ctx.getText());
+                String name = ctx.getText();
+                int line = ctx.start.getLine();
+                int start = ctx.start.getCharPositionInLine();
+                int end = ctx.stop.getCharPositionInLine();
+                addElement(name, line, start, end, "field", elements);
                 return visitChildren(ctx); 
             }
         
         
             @Override public Void visitConstructorDeclarator(Java8Parser.ConstructorDeclaratorContext ctx) { 
-                map.get("methods").add(ctx.getText());
+                String name = ctx.getText();
+                int line = ctx.start.getLine();
+                int start = ctx.start.getCharPositionInLine();
+                int end = ctx.stop.getCharPositionInLine();
+                addElement(name, line, start, end, "constructor", elements);
                 return visitChildren(ctx);  
             }
         
             @Override public Void visitMethodDeclarator(Java8Parser.MethodDeclaratorContext ctx) {
-                map.get("constructors").add(ctx.getText());
+                String name = ctx.getText();
+                int line = ctx.start.getLine();
+                int start = ctx.start.getCharPositionInLine();
+                int end = ctx.stop.getCharPositionInLine();
+                addElement(name, line, start, end, "method", elements);
                 return visitChildren(ctx); 
             }
         }
@@ -63,9 +77,23 @@ class Main {
             e.printStackTrace();
         }
     
-        System.out.print(map.toString());
+        System.out.print(elements.toString());
 
    }
 
-   
+   private static void addElement(String name, int line, int start, int end, String type, List<Map<String, Object>> elements) {
+    JSONObject element = new JSONObject();
+    JSONObject startPos = new JSONObject();
+    JSONObject endPos = new JSONObject();
+    element.put("name", name);
+    startPos.put("line", line);
+    startPos.put("column", start);
+    endPos.put("line", line);
+    endPos.put("column", end);
+    element.put("start", startPos);
+    element.put("end", endPos);
+    element.put("type", type);
+    elements.add(element.toMap());
+    }
+
 }
